5e4b6e426b6898c9f629f20c54bbbc42a48fc69e
Vishal Mule <advvish17@gmail.com>
2025-08-24 13:16:58 +0530

    Creating a query  search for URL

--- controllers/products.js
+++ controllers/products.js
@@ -3,14 +3,16 @@ const Product = require("../models/product")
 const getAllProducts = async (req,res) =>  {
 
     //?  Fetch Data From MongoDB and Product Models
-    const productList = await Product.find({})
+    const productList = await Product.find(req.query)
 
     res.status(200).json( { productList } )
 }
 
 const getAllProductsTesting = async (req,res) =>  {
     
-    const productList = await Product.find({})
+    // % request the Query in URL
+    const productList = await Product.find(req.query)
+
     res.status(200).json( { productList } )
 }
 
