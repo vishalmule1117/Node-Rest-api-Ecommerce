c2764b21828af77874883da9c38d4875a7dfd00a
Vishal Mule <advvish17@gmail.com>
2025-08-24 17:43:23 +0530

    Advanced URL Search of REST API

--- controllers/products.js
+++ controllers/products.js
@@ -2,17 +2,42 @@ const Product = require("../models/product")
 
 const getAllProducts = async (req,res) =>  {
 
-    //?  Fetch Data From MongoDB and Product Models
-    const productList = await Product.find(req.query)
+    const {company, name, featured} = req.query;
+    const queryObject = {};
 
+    if(company){
+        queryObject.company = company;
+    }
+    if(name){
+        //! Advanced search query 
+        queryObject.name = { $regex: name, $options: "i" };
+    }
+    if(featured){
+        queryObject.featured = featured;
+    }
+    //!  Fetch Data From MongoDB and Product Models
+    const productList = await Product.find(queryObject)
     res.status(200).json( { productList } )
 }
 
 const getAllProductsTesting = async (req,res) =>  {
-    
-    // % request the Query in URL
-    const productList = await Product.find(req.query)
 
+    const {company, name, featured} = req.query;
+    const queryObject = {};
+
+    if(company){
+        queryObject.company = company;
+    }
+    if(name){
+        //! Advanced search query 
+        queryObject.name = { $regex: name, $options: "i" };
+    }
+    if(featured){
+        queryObject.featured = featured;
+    }
+    
+    //!  Fetch Data From MongoDB and Product Models
+    const productList = await Product.find(queryObject)
     res.status(200).json( { productList } )
 }
 
--- productDB.js
+++ productDB.js
@@ -1,16 +1,19 @@
-// ? Created connection between mongoDB and product Model
-
+// ? Created connection between mongoDB and product Model 
 require("dotenv").config();
 const connectDB = require("./db/connect");
 const Product = require("./models/product");
-
 const ProductJson = require("./products.json");
 
 const start = async () => {
     try {
+        //! Connect data MongoDB URL
         await connectDB(process.env.MONGODB_URL);
+
+        // ! deleteMany() remove duplicate Entry of JSON data
+        await Product.deleteMany();
         await Product.create(ProductJson);
         console.log("Send Data to mongoDB")
+
     }catch (error){
         console.log(error);
     }
