457b48e48bbc92817eb310fe8d8d84a34d721d2e
Vishal Mule <advvish17@gmail.com>
2025-08-27 17:45:54 +0530

    Add static URL for Third party deployment

--- productDB.js
+++ productDB.js
@@ -7,7 +7,8 @@ const ProductJson = require("./products.json");
 const start = async () => {
     try {
         //! Connect data MongoDB URL
-        await connectDB(process.env.MONGODB_URL);
+        // await connectDB(process.env.MONGODB_URL);
+        await connectDB('mongodb+srv://advvish17:tQZ3tEXCOAaSVaL5@node-rest-api-ecommerce.gilxu7y.mongodb.net/advvish17?retryWrites=true&w=majority&appName=Node-Rest-api-Ecommerce');
 
         // ! deleteMany() remove duplicate Entry of JSON data
         await Product.deleteMany();
