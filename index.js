46312e835953ffd172c63ac47c79568473421fd4
Vishal Mule <advvish17@gmail.com>
2025-08-30 14:25:39 +0530

    added cors URL

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const cors = require('cors')
-app.use(cors());
+app.use(cors({
+  origin : "https://node-rest-api-ecommerce.onrender.com"
+}));
 const connectDB = require("./db/connect");
 
 const PORT = process.env.PORT || 3002;
