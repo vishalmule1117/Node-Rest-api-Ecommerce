70e793134a8d6040d1dfd3604e890772735acb84
Vishal Mule <advvish17@gmail.com>
2025-09-16 17:41:26 +0530

    update totalCount in backend

--- controllers/products.js
+++ controllers/products.js
@@ -42,14 +42,16 @@ const getAllProducts = async (req, res) => {
     let page = Number(req.query.page) || 1;
     let limit = Number(req.query.limit) || 8;
     let skip = (page - 1) * limit;
+
     // Get total count of products
     const totalCount = await productApiData.clone().countDocuments();
-
     productApiData = productApiData.skip(skip).limit(limit);
+
+    res.status(200).json({ status: true, productList, totalCount });
   } else {
     //! Pagination will set default result and Fetch Data From MongoDB Local Compass
     const productList = await productApiData;
-    res.status(200).json({ status: true, productList, totalCount });
+    res.status(200).json({ status: true, productList });
   }
 
   const productList = await productApiData;
