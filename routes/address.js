b1cd023294fb10aeff59bcf081f611ea38da9d35
Vishal Mule <advvish17@gmail.com>
2025-09-23 17:04:03 +0530

    Delete, seDefault address router

--- routes/address.js
+++ routes/address.js
@@ -41,4 +41,57 @@ router.post("/", verifyToken, async (req, res) => {
   }
 });
 
+// get all user address details user Logged in
+router.get("/", verifyToken, async (req, res) => {
+  try {
+    const addresses = await Address.find({ userId: req.user.id });
+    res.json(addresses);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+//! Delete address
+router.delete("/:id", verifyToken, async (req, res) => {
+  try {
+    const address = await address.findOneAndDelete({
+      _id: req.params.id,
+      userId: req.user.id,
+    });
+
+    if (!address) {
+      return res.status(400).json({ message: "Address not found" });
+    }
+    res.json({ message: "Address Deleted" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+//? set default address
+router.put("/default/:id", verifyToken, async (req, res) => {
+  try {
+    //Remove default from other addresses
+    await Address.updateMany(
+      { userId: req.user.id, isDefault: true },
+      { $set: { isDefault: false } }
+    );
+
+    // Set this address as default
+    const updated = await Address.findOneAndUpdate(
+      { _id: req.params.id, userId: req.user.id },
+      { $set: { isDefault: true } },
+      { new: true } // return the updated document
+    );
+
+    if (!updated) {
+      return res.status(400).json({ message: "Address not found" });
+    }
+
+    res.json(updated);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 export default router;
